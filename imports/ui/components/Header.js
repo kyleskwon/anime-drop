54d82117a8fb2a8ad774d3006ee871bf8a38af11
Huan Zhang <huan.zhang@trinitymirror.com>
2016-08-05 17:57:11 +0100

    added basic header + basic header styles file

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -1,11 +1,21 @@
 // @flow
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-export default () => {
-  return (
-    <header>
-      <h1>React Redux Meteor Boilerplate</h1>
-    </header>
-  )
+class Header extends Component {
+  render () {
+    return (
+      <header>
+        <h2 className="site-title">Anime Drop</h2>
+        <nav className="primary">
+          <Link to="/2016/Winter">Winter</Link>
+          <Link to="/2016/Spring">Spring</Link>
+          <Link to="/2016/Summer">Summmer</Link>
+          <Link to="/2016/Fall">Fall</Link>
+        </nav>
+      </header>
+    )
+  }
 }
+
+export default Header
