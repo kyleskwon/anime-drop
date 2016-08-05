903e195c7ed5fc833ee4c150dc6981e44a7c5c41
Huan Zhang <huan.zhang@trinitymirror.com>
2016-08-05 18:13:18 +0100

    changed Header back to dumb component

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -1,21 +1,17 @@
 // @flow
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
-class Header extends Component {
-  render () {
-    return (
-      <header>
-        <h2 className="site-title">Anime Drop</h2>
-        <nav className="primary">
-          <Link to="/2016/Winter">Winter</Link>
-          <Link to="/2016/Spring">Spring</Link>
-          <Link to="/2016/Summer">Summmer</Link>
-          <Link to="/2016/Fall">Fall</Link>
-        </nav>
-      </header>
-    )
-  }
+export default () => {
+  return (
+    <header>
+      <h2 className="site-title">Anime Drop</h2>
+      <nav className="primary">
+        <Link to="/2016/Winter">Winter</Link>
+        <Link to="/2016/Spring">Spring</Link>
+        <Link to="/2016/Summer">Summmer</Link>
+        <Link to="/2016/Fall">Fall</Link>
+      </nav>
+    </header>
+  )
 }
-
-export default Header
