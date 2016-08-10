344249ec1784ae5d095462e65a6a11b0f8175f1c
Jeff Lau <jeff-lau@live.com>
2016-08-10 14:17:29 +0800

    Refactored frontend into separate folder with webpack

--- client/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import router from '../imports/router';
-
-
-Meteor.startup(() => {
-  render(router, document.getElementById('root'));
-})
