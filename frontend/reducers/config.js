391a9d3ed368e439947dddaa733978cfe06ca62b
Jeff Lau <jeff-lau@live.com>
2016-08-23 18:06:43 +0800

    Added dynamic setting of current season

--- frontend/reducers/config.js
+++ frontend/reducers/config.js
@@ -3,15 +3,33 @@ type config = {
   token: ?string
 }
 
-const initialState = {
-  token: null,
-  currentSeason: {
-    year: 2016,
-    season: 'summer'
+const initialState = () => {
+  let now = new Date(),
+      year = now.getFullYear(),
+      month = now.getMonth(),
+      season;
+
+  switch(true){
+    case month >= 0:
+      season = 'winter'
+    case month >= 3:
+      season = 'spring'
+    case month >= 6:
+      season = 'summer'
+    case month >= 9:
+      season = 'autumn'
+  }
+
+  return {
+    token: null,
+    currentSeason: {
+      year,
+      season
+    }
   }
 }
 
-function reducer(state: config = initialState, action: Object = {}){
+function reducer(state: config = initialState(), action: Object = {}){
   switch(action.type) {
     case 'SET_ACCESS_TOKEN':
       return {
