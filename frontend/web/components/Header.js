d1495129fd442da11b037628463d839922047c85
Jeff Lau <jeff-lau@live.com>
2016-08-28 19:07:56 +0800

    Added dynamically changed routes for years

--- frontend/web/components/Header.js
+++ frontend/web/components/Header.js
@@ -4,14 +4,28 @@ import { Link, withRouter } from 'react-router'
 import { connect } from 'react-redux'
 
 class Header extends React.Component {
-  handleRouteChange(season){
-    const { routing, router } = this.props
+  handleSeasonChange(season){
+    const { routing, router, config } = this.props
     let pathname
 
     if(!routing.params.year) {
-      pathname = "/2016/" + season
+      pathname = `/${config.currentSeason.year}/season`
     } else {
       pathname = `/${routing.params.year}/${season}`
+    }
+    router.push({
+      pathname,
+      query: routing.location.query
+    })
+  }
+  handleYearChange(year){
+    const { routing, router, config } = this.props
+    let pathname
+
+    if(!routing.params.season) {
+      pathname = `/${year}/${config.currentSeason.season}`
+    } else {
+      pathname = `/${year}/${routing.params.season}`
     }
     router.push({
       pathname,
@@ -23,14 +37,18 @@ class Header extends React.Component {
       <header>
         <Link to="/"><h2 className="site-title">Anime Drop</h2></Link>
         <nav className="primary">
-          <a onClick={this.handleRouteChange.bind(this, 'winter')}>Winter</a>
-          <a onClick={this.handleRouteChange.bind(this, 'spring')}>Spring</a>
-          <a onClick={this.handleRouteChange.bind(this, 'summer')}>Summer</a>
-          <a onClick={this.handleRouteChange.bind(this, 'fall')}>Fall</a>
+          <a onClick={this.handleSeasonChange.bind(this, 'winter')}>Winter</a>
+          <a onClick={this.handleSeasonChange.bind(this, 'spring')}>Spring</a>
+          <a onClick={this.handleSeasonChange.bind(this, 'summer')}>Summer</a>
+          <a onClick={this.handleSeasonChange.bind(this, 'fall')}>Fall</a>
+          <a onClick={this.handleYearChange.bind(this, 2016)}>2016</a>
+          <a onClick={this.handleYearChange.bind(this, 2015)}>2015</a>
+          <a onClick={this.handleYearChange.bind(this, 2014)}>2014</a>
+          <a onClick={this.handleYearChange.bind(this, 2013)}>2013</a>
         </nav>
       </header>
     )
   }
 }
 
-export default connect(({ routing }) => ({ routing }))(withRouter(Header))
+export default connect(({ routing, config }) => ({ routing, config }))(withRouter(Header))
