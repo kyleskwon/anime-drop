947db93a99cf0ddbf8ecfc3afd5b02ab58c11b29
Huan Zhang <huan.zhang@trinitymirror.com>
2016-09-04 03:13:59 +0100

    added function to capitalise first letter of every word

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,3 +1,8 @@
-const formatScore = (score) => (Math.round(parseInt(score, 10)))/10
+const formatScore = score => (Math.round(parseInt(score, 10)))/10
 
-export default formatScore;
+const capitalise = str => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+
+export {
+  formatScore,
+  capitalise
+}
