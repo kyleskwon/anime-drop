ac4ce3c3e6ffc702bf8fb35b401aa2facf28d4df
Jeff Lau <jeff-lau@live.com>
2016-09-04 15:07:53 +0800

    Updated jest-cli

--- meteor/imports/anilistScraper.js
+++ meteor/imports/anilistScraper.js
@@ -1,7 +1,12 @@
 // @flow
+import AL from './api/anilist'
 
+const years = [2017, 2016, 2015, 2014, 2013]
+const season = ['winter', 'spring', 'summer', 'fall']
 
+function getAllSeasons(years, seasons){
 
+}
 //go get all by season and add them to the database
 
 
